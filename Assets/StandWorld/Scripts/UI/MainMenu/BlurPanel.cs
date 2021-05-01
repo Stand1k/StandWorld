6ca853efe3943840e3d0121c15caef5c0bda7579
Denis Yatsenko <yatsenko.denis46@gmail.com>
2021-05-01 19:40:40 +0300

    Add main menu
    Add blur background shader and init main menu

---
+++ Assets/StandWorld/Scripts/UI/MainMenu/BlurPanel.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace StandWorld.UI.MainMenu
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    [AddComponentMenu("UI/Blur Panel")]
+    public class BlurPanel : Image
+    {
+        public float time = 0.5f;
+        public float delay = 0;
+
+        private CanvasGroup _canvasGroup;
+
+        protected override void Awake()
+        {
+            _canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            
+            if (Application.isPlaying)
+            {
+                color = Color.black * 0.1f;
+                material.SetFloat("_Size", 0);
+                _canvasGroup.alpha = 0f;
+                LeanTween.value(gameObject, UpdateBlur, 0, 3, time).setDelay(delay);
+            }
+        }
+
+        void UpdateBlur(float value)
+        {
+            material.SetFloat("_Size", value);
+            _canvasGroup.alpha = value;
+        }
+    }
+}
---
+++ Assets/StandWorld/Scripts/UI/MainMenu/Editor/BlurPanelEditor.cs
@@ -0,0 +1,24 @@
+﻿using StandWorld.UI.MainMenu;
+using UnityEditor;
+using UnityEditor.UI;
+
+namespace StandWorld.UI.MainMenu
+{
+    [CustomEditor((typeof(BlurPanel)))]
+    public class BlurPanelEditor : ImageEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("time"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("delay"));
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+    }
+}
